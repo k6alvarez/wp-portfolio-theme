b77c50822816ddc2ae43d816c0af4ec75c0faf4c
k6alvarez <k6alvarez@gmail.com>
2015-09-06 19:17:19 -0400

    Added modal to featured work area

--- wordpress/wp-content/themes/kalvarez-web-portfolio/js/site.js
+++ wordpress/wp-content/themes/kalvarez-web-portfolio/js/site.js
@@ -1,25 +1,62 @@
 jQuery(document).ready(function($) {
-  var stars=800;
-  var $stars=$("#stars");
-  var r=800;
-  for(var i=0;i<stars;i++){
-    var $star=$("<div/>").addClass("star");
-    $stars.append($star);
-  }
-  $(".star").each(function(){
-    var cur=$(this);
-    var s=0.2+(Math.random()*1);
-    var curR=r+(Math.random()*300);
-    cur.css({ 
-      transformOrigin:"0 0 "+curR+"px",
-      transform:" translate3d(0,0,-"+curR+"px) rotateY("+(Math.random()*360)+"deg) rotateX("+(Math.random()*-50)+"deg) scale("+s+","+s+")"
-       
-    });
-  });
+	var stars=800;
+	var $stars=$("#stars");
+	var r=800;
+	for(var i=0;i<stars;i++){
+		var $star=$("<div/>").addClass("star");
+		$stars.append($star);
+	}
+	$(".star").each(function(){
+		var cur=$(this);
+		var s=0.2+(Math.random()*1);
+		var curR=r+(Math.random()*300);
+		cur.css({ 
+		  transformOrigin:"0 0 "+curR+"px",
+		  transform:" translate3d(0,0,-"+curR+"px) rotateY("+(Math.random()*360)+"deg) rotateX("+(Math.random()*-50)+"deg) scale("+s+","+s+")"
+		   
+		});
+	});
 
-  //thumbnails
-  $('.thumbnail a').on('click',function(e){
-  	e.preventDefault();
-  });
+
+	var overlayModal = {
+		content: 'No content passed',
+		// numProp: 4,
+		// bProp: true,
+		// arrayLiteral: ['value1','value2','value3','value4'],
+		// pos:{ //nested object literal
+		// 	x: 40,
+		// 	y: 300
+		// },
+		onStart: function(content){ //function
+			// console.log(overlayModal.content);
+			overlayModal.content = content;
+			overlayModal.setUp();
+		},
+		setUp: function() {
+			// console.log(overlayModal.content);
+			var modalMarkup = '<div class="modal"><a href="#" id="close">X Close</a><div class="container">';
+			modalMarkup += overlayModal.content;
+			modalMarkup += '</div></div>';
+			$('body').addClass('modal-on').append(modalMarkup);
+		},
+		kill: function(){
+			$('body').removeClass('modal-on');
+			$('div.modal').remove();
+		}
+
+	};
+
+	//thumbnails
+	$('.thumbnail a.trigger').on('click',function(e){
+		e.preventDefault();
+		var esto = $(this);	
+		var content = esto.parent().html();
+		overlayModal.onStart(content);
+	});
+
+	$('#close').live('click',function(e){
+		e.preventDefault();
+		overlayModal.kill();
+	});
 
 });
--- wordpress/wp-content/themes/kalvarez-web-portfolio/template-parts/content-page.php
+++ wordpress/wp-content/themes/kalvarez-web-portfolio/template-parts/content-page.php
@@ -26,48 +26,48 @@
 			<div class="container">
 				<h1>Featured Work</h1>
 				<p>Here you can read about some of the projects I have worked on. I'm always looking to take on short fun projects in my free time.</p>
-				<div class="thumbnail">
-					<a href="#"><img src="http://localhost:8888/wp-content/uploads/2015/09/penguins.jpg" alt=""></a>
+				<div class="thumbnail">					
 					<div class="more-info">
 						<p class="title">Penguin Naming Contest - <span class="category">Facebook App</span></p>
 						<p class="description">This project was a Facebook campaign for the Georgia Aquarium that created a voting contest to pick the names of two new baby penguins. This project was built using HTML, CSS, JQuery and was implemented using ShortStack. The project won an  AMY award for Web & Interactive Marketing.</p>						
-					</div>					
+					</div>				
+					<a class="trigger" href="#"><img src="http://localhost:8888/wp-content/uploads/2015/09/penguins.jpg" alt=""></a>	
 				</div>
-				<div class="thumbnail">
-					<a href="#"><img src="http://localhost:8888/wp-content/uploads/2015/09/black-21.jpg" alt=""></a>					
+				<div class="thumbnail">					
 					<div class="more-info">
 						<p class="title">21 Black Series - <span class="category">Website</span></p>
 						<p class="description">I did the front end work on this micro-site for the Georgia Lottery. The site is built using HTML, CSS, JQuery.</p>						
 					</div>
+					<a class="trigger" href="#"><img src="http://localhost:8888/wp-content/uploads/2015/09/black-21.jpg" alt=""></a>					
 				</div>
-				<div class="thumbnail last">
-					<a href="#"><img src="http://localhost:8888/wp-content/uploads/2015/09/makeup-artist.jpg" alt=""></a>
+				<div class="thumbnail last">					
 					<div class="more-info">
 						<p class="title">Makeup Artist Theme - <span class="category">Wordpress</span></p>
 						<p class="description">Simple wordpress theme for a makeup artist or any artist that wants show of some their work. I designed and developed this theme.</p>
 					</div>
+					<a class="trigger" href="#"><img src="http://localhost:8888/wp-content/uploads/2015/09/makeup-artist.jpg" alt=""></a>
 				</div>
-				<div class="thumbnail">
-					<a href="#"><img src="http://localhost:8888/wp-content/uploads/2015/09/cherrybomb.jpg" alt=""></a>
+				<div class="thumbnail">					
 					<div class="more-info">
 						<p class="title">Cherry Bomb Dance - <span class="category">Wordpress</span></p>
 						<p class="description">If you have a wordpress theme that you like and need it set up, I can do that for you too. I installed and set up this wordpress theme for the Cherry Bomb Dance group.</p>						
 					</div>
+					<a class="trigger" href="#"><img src="http://localhost:8888/wp-content/uploads/2015/09/cherrybomb.jpg" alt=""></a>
 				</div>		
-				<div class="thumbnail ">
-					<a href="#"><img src="http://localhost:8888/wp-content/uploads/2015/09/trane-mobile.jpg" alt=""></a>
+				<div class="thumbnail ">					
 					<div class="more-info">
 						<p class="title">Trane Commercial - <span class="category">Website</span></p>
 						<p class="description">Created front end prototype and worked with client's in house back end team to implement the new site.</p>						
 					</div>
+					<a class="trigger" href="#"><img src="http://localhost:8888/wp-content/uploads/2015/09/trane-mobile.jpg" alt=""></a>
 				</div>				
 						
-				<div class="thumbnail last">
-					<a href="#"><img src="http://localhost:8888/wp-content/uploads/2015/09/truckerapp.jpg" alt=""></a>
+				<div class="thumbnail last">					
 					<div class="more-info">
 						<p class="title">Trucker App - <span class="category">Cordova Android App</span></p>
 						<p class="description">My job on this app was to integrated the google maps api to allows users to plan their route. This was one of my first projects.</p>						
 					</div>
+					<a class="trigger" href="#"><img src="http://localhost:8888/wp-content/uploads/2015/09/truckerapp.jpg" alt=""></a>
 				</div>				
 			</div>
 		</div>
